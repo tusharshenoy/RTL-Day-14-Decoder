18a9df0d8d1786dd77a9dbe8971f91b45247fb03
T Tushar Shenoy <107348474+tusharshenoy@users.noreply.github.com>
2023-10-03 18:37:31 +0530

    Update Decoder4to16_tb.v

--- Decoder4to16_tb.v
+++ Decoder4to16_tb.v
@@ -2,22 +2,18 @@
 module Decoder4to16_tb();
 
 reg [3:0]A;
-reg E;
 wire [15:0]Y;
 integer i;
-Decoder4to16 dut(.A(A),.E(E),.Y(Y));
+Decoder4to16 dut(.A(A),.Y(Y));
 
 initial begin
-   E=1'b0;
-   A=4'bxxxx;
-#5 E=1'b1;
    A=4'b1111;
   for(i=0;i<16;i=i+1)
   begin
   A=A+1;
   #5;
   end
-#5 $finish;
+$finish;
 end
 
 endmodule
